92315744bc3c43f8b5f9e76c1b8463362a64b429
Kreidina <kav99mail@gmail.com>
2023-09-19 18:52:07 +0300

    fix

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+// import Header from "../Header/Header";
+
+const Layout = () => {
+  return (
+    <>
+      <header>{/* <Header /> */}</header>
+      <main>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
+};
+
+export default Layout;
