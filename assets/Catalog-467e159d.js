d51b3335acd14ee6b8e0ddf940dfe24f3e2b4190
Kreidina <Kreidina@users.noreply.github.com>
2023-09-19 17:50:27 +0000

    deploy: 977a5b5a8c61149aaeb60ea96e7760d35cd1d8ba

--- assets/Catalog-467e159d.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-d0102fb0.js";const s=()=>t.jsx("div",{children:"List"});export{s as default};
--- assets/Favorite-86864fc3.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-d0102fb0.js";const a=()=>t.jsx(t.Fragment,{children:"Catalog"});export{a as default};
